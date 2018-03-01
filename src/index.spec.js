b3e10fc54ed770749a79d462245f4c80c1b5e68e
Dominik Grusemann <dominik.grusemann@gmail.com>
2018-03-01 12:19:45 +0100

    add tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -5,5 +5,11 @@ describe('ip', function() {
     it('should set the last octet to 0', function() {
       expect(gdpr.anonymizeIp('127.0.0.1')).to.eq('127.0.0.0')
     });
+    it('does not fail when it is not an ip address', function() {
+      expect(gdpr.anonymizeIp('This is a test.')).to.eq('This is a test.')
+    });
+    it('does not fail when it is empty', function() {
+      expect(gdpr.anonymizeIp('')).to.eq('')
+    });
   });
 });
