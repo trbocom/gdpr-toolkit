b32dd6284e3c90c34aa460a5ddef73a016a56c1d
Dominik Grusemann <dominik.grusemann@gmail.com>
2018-03-01 12:02:33 +0100

    change signature

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,9 +1,9 @@
-const ip = require('./ip')
+const gdpr = require('./gdpr')
 
 describe('ip', function() {
   describe('#anonymize()', function() {
     it('should set the last octet to 0', function() {
-      expect(ip.anonymize('127.0.0.1')).to.eq('127.0.0.0')
+      expect(gdpr.anonymizeIp('127.0.0.1')).to.eq('127.0.0.0')
     });
   });
 });
