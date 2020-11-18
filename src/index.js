c7f7788afe3ed5532920f4ba17b6ef6f902229d0
dgrusemann <dominik.grusemann@gmail.com>
2020-11-18 15:52:06 +0100

    refactor - use guard style

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 module.exports = {
   anonymizeIp (ip) {
-    if (ip) {
-      const octets = ip.split('.')
-      if (octets.length < 4) return ip
-      return octets[0] + '.' + octets[1] + '.' + octets[2] + '.' + '0'
-    } else {
-      return ip
-    }
+    if (!ip) return ip
+
+    const octets = ip.split('.')
+    if (octets.length < 4) return ip
+    return octets[0] + '.' + octets[1] + '.' + octets[2] + '.' + '0'
   }
 }
