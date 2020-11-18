cbab0fff7c11165a7371d17e9a7c9a7cfc06cc1c
dgrusemann <dominik.grusemann@gmail.com>
2020-11-18 15:51:09 +0100

    refactor - formatting

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ module.exports = {
   anonymizeIp (ip) {
     if (ip) {
       const octets = ip.split('.')
-      if (octets.length < 4) {
-        return ip
-      }
+      if (octets.length < 4) return ip
       return octets[0] + '.' + octets[1] + '.' + octets[2] + '.' + '0'
     } else {
       return ip
