f73bfd4819772ce9b1b75da27b50ac90bb59558e
dgrusemann <dominik.grusemann@gmail.com>
2022-08-29 15:53:05 +0200

    feat: anonymize ipv6 (unshortened) [sc13864]

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+function anonymizeIpv4 (ip) {
+  const octets = ip.split('.')
+  if (octets.length < 4) return ip
+
+  return octets[0] + '.' + octets[1] + '.' + octets[2] + '.' + '0'
+}
+
+function anonymizeIpv6 (ip) {
+  const hexs = ip.split(':')
+  if (hexs.length < 8) return ip
+
+  return hexs[0] + ':' + hexs[1] + ':' + hexs[2] + ':' + hexs[3] + ':' + '0:0:0:0'
+}
+
 module.exports = {
   anonymizeIp (ip) {
     if (!ip) return ip
 
-    const octets = ip.split('.')
-    if (octets.length < 4) return ip
-    return octets[0] + '.' + octets[1] + '.' + octets[2] + '.' + '0'
+    if (ip.indexOf('.') !== -1) return anonymizeIpv4(ip)
+    if (ip.indexOf(':') !== -1) return anonymizeIpv6(ip)
+    return ip
   }
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -2,17 +2,24 @@ const gdpr = require('./index')
 
 describe('ip', function () {
   describe('#anonymize()', function () {
-    it('should set the last octet to 0', function () {
+    it('sets the last octet of an IPv4 address to 0', function () {
       expect(gdpr.anonymizeIp('127.0.0.1')).to.eq('127.0.0.0')
     })
+
     it('does not fail when it is not an ip address', function () {
       expect(gdpr.anonymizeIp('This is a test.')).to.eq('This is a test.')
     })
+
     it('does not fail when it is empty', function () {
       expect(gdpr.anonymizeIp('')).to.eq('')
     })
+
     it('does not fail when it is undefined', function () {
       expect(gdpr.anonymizeIp(undefined)).to.eq(undefined)
+    })
+
+    it('sets the last 64 bits of an unshortened IPv6 address to 0', function () {
+      expect(gdpr.anonymizeIp('2a02:810d:f00:7498:2492:dc94:82a4:9203')).to.eq('2a02:810d:f00:7498:0:0:0:0')
     })
   })
 })
