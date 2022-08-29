165eb4a485bd42ec0b5e629fe70660daf08c90b2
dgrusemann <dominik.grusemann@gmail.com>
2022-08-29 16:08:39 +0200

    test: add contexts [sc13864]

--- src/index.spec.js
+++ src/index.spec.js
@@ -2,28 +2,32 @@ const gdpr = require('./index')
 
 describe('ip', function () {
   describe('#anonymize()', function () {
-    it('sets the last octet of an IPv4 address to 0', function () {
-      expect(gdpr.anonymizeIp('127.0.0.1')).to.eq('127.0.0.0')
+    context('ipv4', function () {
+      it('sets the last octet of an IPv4 address to 0', function () {
+        expect(gdpr.anonymizeIp('127.0.0.1')).to.eq('127.0.0.0')
+      })
+
+      it('does not fail when it is not an ip address', function () {
+        expect(gdpr.anonymizeIp('This is a test.')).to.eq('This is a test.')
+      })
+
+      it('does not fail when it is empty', function () {
+        expect(gdpr.anonymizeIp('')).to.eq('')
+      })
+
+      it('does not fail when it is undefined', function () {
+        expect(gdpr.anonymizeIp(undefined)).to.eq(undefined)
+      })
     })
 
-    it('does not fail when it is not an ip address', function () {
-      expect(gdpr.anonymizeIp('This is a test.')).to.eq('This is a test.')
-    })
+    context('ipv6', function () {
+      it('sets the last 64 bits of an unshortened IPv6 address to 0', function () {
+        expect(gdpr.anonymizeIp('2a02:810d:f00:7498:2492:dc94:82a4:9203')).to.eq('2a02:810d:f00:7498:0:0:0:0')
+      })
 
-    it('does not fail when it is empty', function () {
-      expect(gdpr.anonymizeIp('')).to.eq('')
-    })
-
-    it('does not fail when it is undefined', function () {
-      expect(gdpr.anonymizeIp(undefined)).to.eq(undefined)
-    })
-
-    it('sets the last 64 bits of an unshortened IPv6 address to 0', function () {
-      expect(gdpr.anonymizeIp('2a02:810d:f00:7498:2492:dc94:82a4:9203')).to.eq('2a02:810d:f00:7498:0:0:0:0')
-    })
-
-    it('sets the last 64 bits of a shortened IPv6 address to 0', function () {
-      expect(gdpr.anonymizeIp('2001:db8::8a2e:370:7334')).to.eq('2001:db8:0:0:0:0:0:0')
+      it('sets the last 64 bits of a shortened IPv6 address to 0', function () {
+        expect(gdpr.anonymizeIp('2001:db8::8a2e:370:7334')).to.eq('2001:db8:0:0:0:0:0:0')
+      })
     })
   })
 })
