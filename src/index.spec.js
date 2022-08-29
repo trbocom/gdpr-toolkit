59c8d59204c493d0b3413392fe77e837cb72fc7b
dgrusemann <dominik.grusemann@gmail.com>
2022-08-29 17:47:06 +0200

    test: change ipv6 addresses [sc13864]

--- src/index.spec.js
+++ src/index.spec.js
@@ -22,23 +22,23 @@ describe('ip', function () {
 
     context('ipv6', function () {
       it('sets the last 64 bits of an unshortened IPv6 address to 0', function () {
-        expect(gdpr.anonymizeIp('2a02:810d:f00:7498:2492:dc94:82a4:9203')).to.eq('2a02:810d:f00:7498:0:0:0:0')
+        expect(gdpr.anonymizeIp('e2ae:3624:7586:f0c4:5b14:39f0:1560:2e83')).to.eq('e2ae:3624:7586:f0c4:0:0:0:0')
       })
 
       it('sets the last 64 bits of a shortened (beginning) IPv6 address to 0', function () {
-        expect(gdpr.anonymizeIp('::2001:4860:4860:0')).to.eq('0:0:0:0:0:0:0:0')
+        expect(gdpr.anonymizeIp('::5b14:39f0:1560:2e83')).to.eq('0:0:0:0:0:0:0:0')
       })
 
       it('sets the last 64 bits of a shortened (middle) IPv6 address to 0', function () {
-        expect(gdpr.anonymizeIp('2001:db8::8a2e:370:7334')).to.eq('2001:db8:0:0:0:0:0:0')
+        expect(gdpr.anonymizeIp('e2ae:3624::39f0:1560:2e83')).to.eq('e2ae:3624:0:0:0:0:0:0')
       })
 
       it('sets the last 64 bits of a shortened (end) IPv6 address to 0', function () {
-        expect(gdpr.anonymizeIp('2001:4860:4860::')).to.eq('2001:4860:4860:0:0:0:0:0')
+        expect(gdpr.anonymizeIp('e2ae:3624:7586::')).to.eq('e2ae:3624:7586:0:0:0:0:0')
       })
 
       it('does not fail with invalid shortened addresses', function () {
-        expect(gdpr.anonymizeIp('2a02:810d:f00:7498::2492:dc94:82a4:9203:9203')).to.eq('2a02:810d:f00:7498:0:0:0:0')
+        expect(gdpr.anonymizeIp('e2ae:3624:7586:f0c4::5b14:39f0:1560:2e83:2e83')).to.eq('e2ae:3624:7586:f0c4:0:0:0:0')
       })
     })
   })
