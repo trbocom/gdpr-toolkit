b9f8753cb154ffbcae27a7a146591b08266530a0
Dominik <dominik.grusemann@gmail.com>
2022-08-30 10:11:44 +0200

    Anonymize ip v6 (#12)
    * feat: anonymize ipv6 (unshortened) [sc13864]
    
    * feat: anonymize ipv6 (shortened) [sc13864]
    
    * test: add contexts [sc13864]
    
    * test: add spec [sc13864]
    
    * feat: anonymize ipv6 (shortened) [sc13864]
    
    * chore: make sure it doesn't fail [sc13864]
    
    * Add .circleci/config.yml
    
    * build: adapt circleci config [sc13864]
    
    * build: adapt circleci config [sc13864]
    
    * build: use node image [sc13864]
    
    * build: use node image [sc13864]
    
    * build: yarn install [sc13864]
    
    * build: fix config [sc13864]
    
    * docs: update README [sc13864]
    
    * docs: update README [sc13864]
    
    * test: change ipv6 addresses [sc13864]

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
+function anonymizeIpv4 (ip) {
+  const octets = ip.split('.')
+  if (octets.length < 4) return ip
+
+  return octets[0] + '.' + octets[1] + '.' + octets[2] + '.' + '0'
+}
+
+function unshortenIpv6 (ip) {
+  if (ip.indexOf('::') === -1) return ip
+  if (ip.indexOf('::') === 0) ip = '0' + ip
+
+  const groups = ip.split(/[0-9a-f]:[0-9a-f]/).length + 1
+  if (groups > 8) return ip
+
+  return ip.replace('::', ':0'.repeat(8 - groups) + ':')
+}
+
+function anonymizeIpv6 (ip) {
+  ip = unshortenIpv6(ip)
+  const groups = ip.split(':')
+  if (groups.length < 8) return ip
+
+  return groups[0] + ':' + groups[1] + ':' + groups[2] + ':' + groups[3] + ':' + '0:0:0:0'
+}
+
 module.exports = {
   anonymizeIp (ip) {
     if (!ip) return ip
 
-    const octets = ip.split('.')
-    if (octets.length < 4) return ip
-    return octets[0] + '.' + octets[1] + '.' + octets[2] + '.' + '0'
+    if (ip.indexOf('.') !== -1) return anonymizeIpv4(ip)
+    if (ip.indexOf(':') !== -1) return anonymizeIpv6(ip)
+    return ip
   }
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -2,17 +2,44 @@ const gdpr = require('./index')
 
 describe('ip', function () {
   describe('#anonymize()', function () {
-    it('should set the last octet to 0', function () {
-      expect(gdpr.anonymizeIp('127.0.0.1')).to.eq('127.0.0.0')
+    context('ipv4', function () {
+      it('sets the last octet of an IPv4 address to 0', function () {
+        expect(gdpr.anonymizeIp('127.0.0.1')).to.eq('127.0.0.0')
+      })
+
+      it('does not fail when it is not an ip address', function () {
+        expect(gdpr.anonymizeIp('This is a test.')).to.eq('This is a test.')
+      })
+
+      it('does not fail when it is empty', function () {
+        expect(gdpr.anonymizeIp('')).to.eq('')
+      })
+
+      it('does not fail when it is undefined', function () {
+        expect(gdpr.anonymizeIp(undefined)).to.eq(undefined)
+      })
     })
-    it('does not fail when it is not an ip address', function () {
-      expect(gdpr.anonymizeIp('This is a test.')).to.eq('This is a test.')
-    })
-    it('does not fail when it is empty', function () {
-      expect(gdpr.anonymizeIp('')).to.eq('')
-    })
-    it('does not fail when it is undefined', function () {
-      expect(gdpr.anonymizeIp(undefined)).to.eq(undefined)
+
+    context('ipv6', function () {
+      it('sets the last 64 bits of an unshortened IPv6 address to 0', function () {
+        expect(gdpr.anonymizeIp('e2ae:3624:7586:f0c4:5b14:39f0:1560:2e83')).to.eq('e2ae:3624:7586:f0c4:0:0:0:0')
+      })
+
+      it('sets the last 64 bits of a shortened (beginning) IPv6 address to 0', function () {
+        expect(gdpr.anonymizeIp('::5b14:39f0:1560:2e83')).to.eq('0:0:0:0:0:0:0:0')
+      })
+
+      it('sets the last 64 bits of a shortened (middle) IPv6 address to 0', function () {
+        expect(gdpr.anonymizeIp('e2ae:3624::39f0:1560:2e83')).to.eq('e2ae:3624:0:0:0:0:0:0')
+      })
+
+      it('sets the last 64 bits of a shortened (end) IPv6 address to 0', function () {
+        expect(gdpr.anonymizeIp('e2ae:3624:7586::')).to.eq('e2ae:3624:7586:0:0:0:0:0')
+      })
+
+      it('does not fail with invalid shortened addresses', function () {
+        expect(gdpr.anonymizeIp('e2ae:3624:7586:f0c4::5b14:39f0:1560:2e83:2e83')).to.eq('e2ae:3624:7586:f0c4:0:0:0:0')
+      })
     })
   })
 })
